c51b0659e8666c2f710e97cfbeba48b0d739a1b0
g1st <gintstan@gmail.com>
2018-02-12 21:18:57 +0000

    moving forward

--- public/javascripts/entry.js
+++ public/javascripts/entry.js
@@ -1,3 +1,15 @@
-import './modules/highstock.js';
+import highstock from './modules/highstock.js';
+import getStockData from './modules/getData.js';
 
-console.log(`hillow`);
+getStockData(
+  `https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?column_index=4&start_date=2018-01-01&end_date=2018-02-12&collapse=daily&order=asc&api_key=tEUBaD69NkzPc6io288M`
+);
+
+// cia callina frontend function pachekint db kokios akcijos yra
+// tada kia gauna su jomis callina akciju api
+// gave statsus piesia grapha
+
+// papildomos funkcijos kai prideda viena akcija, tai frontende piesia akcija
+// callina akciju api, gauna info ir piesia grapha
+// prideda akcija i db
+// bet josj au viduj kitu, ne ant sito main screeno
