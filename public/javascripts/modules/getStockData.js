6dbff84f8fadbaaf734cde8bb3ef1976c188cde9
g1st <gintstan@gmail.com>
2018-02-15 21:25:14 +0000

    get stocks from db atm

--- public/javascripts/modules/getStockData.js
+++ public/javascripts/modules/getStockData.js
@@ -1,19 +1,57 @@
 import axios from 'axios';
 import drawChart from './drawChart.js';
+import moment from 'moment';
+moment().format();
 
-const url = `https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json?column_index=4&start_date=2018-01-01&end_date=2018-02-12&collapse=daily&order=asc&api_key=tEUBaD69NkzPc6io288M`;
+const endDate = moment().format('YYYY-MM-DD');
 
-const getStockData = async url => {
+const startDate = moment(endDate)
+  .subtract(1, 'years')
+  .format('YYYY-MM-DD');
+
+const getStockData = async () => {
   try {
-    const response = await axios.get(url);
-    const data = response.data.dataset;
-    console.log(data.data);
-    const options = [
-      { name: 'USD to EUR', data: [1, 2, 3, 4, 5, 6, 5, 4, 3, 9] },
-      { name: data.dataset_code, data: data.data }
-    ];
+    const stocks = await getAllStocksFromDb();
+
+    const stocksArr = [];
+    stocks.forEach(stock => {
+      const url = `https://www.quandl.com/api/v3/datasets/WIKI/${stock}.json?column_index=4&start_date=${startDate}&end_date=${endDate}&collapse=daily&order=asc&api_key=tEUBaD69NkzPc6io288M`;
+      stocksArr.push(function() {
+        return axios.get(url);
+      });
+    });
+
+    console.log(stocksArr);
+
+    // for (const stock of stocks) {
+    //   console.log(stock);
+    // const url = `https://www.quandl.com/api/v3/datasets/WIKI/${stock}.json?column_index=4&start_date=${startDate}&end_date=${endDate}&collapse=daily&order=asc&api_key=tEUBaD69NkzPc6io288M`;
+    //   const response = await axios.get(url);
+    //   const data = response.data.dataset.data;
+    //   const name = response.data.dataset.dataset_code;
+    //   console.log(data, name);
+    // }
+
+    // const options = [
+    //   // { name: 'USD to EUR', data: [1, 2, 3, 4, 5, 6, 5, 4, 3, 9] },
+    //   { name, data }
+    // ];
     // draw chart here, when all data fetched
     drawChart(options);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getAllStocksFromDb = async () => {
+  try {
+    const stocksInDb = await axios.get('stock/all');
+
+    const stocksSet = new Set();
+    for (const stock of stocksInDb.data.stocks) {
+      stocksSet.add(stock.stock);
+    }
+    return stocksSet;
   } catch (error) {
     console.log(error);
   }
--- routes/stock.js
+++ routes/stock.js
@@ -1,24 +1,56 @@
+const axios = require('axios');
+
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Stock = require('../models/stocks');
+const moment = require('moment');
+moment().format();
+
+// url to check if stock exists
 
 /* handle POST stock request */
 router.post('/', async function(req, res, next) {
   try {
     const stockName = req.body.stock;
+    const currentMonthEnd = moment()
+      .endOf('month')
+      .format('YYYY-MM-DD');
+    const previousMonthEnd = moment(currentMonthEnd)
+      .add(1, 'months')
+      .endOf('month')
+      .format('YYYY-MM-DD');
+
+    const url = `https://www.quandl.com/api/v3/datasets/WIKI/${stockName}.json?start_date=${currentMonthEnd}&end_date=${previousMonthEnd}&collapse=monthly&api_key=${
+      process.env.quandl_api_key
+    }`;
+
+    try {
+      const stockExists = await axios.get(url);
+    } catch (error) {
+      // gali buti vieno catch gana, paziet ka returnina galiorkoj ir pagal tai sugaudyt kada invaldi name, o kadan ormalus error?
+      return res.status(404).json({ error: `Invalid stock name` });
+    }
+
     const oneStock = await Stock.find({ stock: stockName });
     if (oneStock.length) {
       // stock already exsists
       return res.status(200).json({ message: 'Stock already exists' });
     }
-    // before saving need to check if it exists in stocks db
-    // validStockName(stockName)
     const stock = await new Stock({ stock: stockName }).save();
     const Stocks = await Stock.find({});
-    console.log('Stocks - ', Stocks);
+
     // 201 = created successfully
     res.status(201).json({ stocks: Stocks });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get('/all', async function(req, res) {
+  try {
+    const Stocks = await Stock.find({});
+    return res.status(200).json({ stocks: Stocks });
   } catch (error) {
     console.log(error);
   }
